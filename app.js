132584e3bd1ba2f1feaabe291b6ea6675a68c57c
“Steve <graphicdesigner@gmail.com>
2018-04-26 19:43:44 -0400

    initial ui

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ // ----------From Google Maps ----------
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+// ---------- Using the homework files to make this work ----------
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
