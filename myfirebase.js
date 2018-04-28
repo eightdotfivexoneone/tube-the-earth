25e0143b57c92cf1026bc058d4faa4bf74d4853c
Molly Zurheide <you@example.com>
2018-04-28 13:07:35 -0400

    added document.ready

--- myfirebase.js
+++ myfirebase.js
@@ -15,17 +15,18 @@ var database = firebase.database();
 
 // Initial values
 var userAddress = "";
-
+$(document).ready(function() {
 $("#search-button").on("click", function() {
-    console.log("hi there");
+    //console.log("hi there");
     userAddress = $("#search-field").val().trim();
     database.ref().set({
         userAddress: userAddress
     });
     database.ref().on("value", function(snapshot) {
         console.log(snapshot.val().userAddress);
-        console.log("hello");
+        //console.log("hello");
         var userLocation = snapshot.val().userAddress;
         console.log(userLocation)
     });
+});
 })
