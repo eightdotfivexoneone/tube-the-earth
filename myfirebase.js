05f8af586f54b64d1940d67373c5c55ad6203cca
Molly Zurheide <you@example.com>
2018-04-30 21:28:45 -0400

    more updates

--- myfirebase.js
+++ myfirebase.js
@@ -15,7 +15,8 @@ var database = firebase.database();
 
 // Initial values
 var userAddress = "";
-//var userLocationSearch = "";
+var numChildren = 0;
+var childrenArray = [];
 
 $(document).ready(function() {
 $("#search-button").on("click", function() {
@@ -23,17 +24,21 @@ $("#search-button").on("click", function() {
     userAddress = $("#search-field").val().trim();
     database.ref().push({
         userAddress: userAddress, //add jesse's var for location
-        //userLocationSearch: userAddress //storing this data to firebase
     });
 });
 
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val().userAddress)
-    //userLocationSearch = snapshot.val().userAddress;
-    //console.log(userAddress); //this isn't being added to "popular" for subsequent users
-    $("#popular").append(childSnapshot.val().userAddress + ", ") //HOW TO PUSH WHAT'S IN DATABASE TO "POPULAR" FIELD????????????
+database.ref().on("child_added", function(childSnapshot) { //each time child is added to database...
+    numChildren++; //increment # of children by one
+    childrenArray.push(childSnapshot.val().userAddress); //push the new location the user entered to childrenArray
+    if (numChildren < 5) { //if there are fewer than 5 children in database...
+        $("#popular").append(childSnapshot.val().userAddress + ", ") //push user's location search to page
+    } else if (numChildren >= 10) { //otherwise, if there are 10+ children in database...
+        $(childrenArray[0]).replaceWith(childSnapshot.val().userAddress); //replace oldest item in childrenArray with latest one
+        $("#popular").html(childrenArray.join(',')); //push updated contents of childrenArray to page BUT NEED COMMAS TO SEPARATE
+    }
+    console.log(childrenArray)
 });
     
-    }); //if < 5 items in database, push; if 10+, replace oldest item (set)!!!!!!!!!!!!!!!!!!!!!
+    }); //if 10+, replace oldest item (set)!!!!!!!!!!!!!!!!!!!!!
     
     //GET VIDEO THUMBNAIL IMG TO STAY ON PAGE UPON REFRESH -- look into json.parse
