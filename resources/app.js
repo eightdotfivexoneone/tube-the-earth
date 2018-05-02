0b9810e5e2308896b13d17f01e2c56268e6d1267
Matt Eisenstadter <mattieice@gmail.com>
2018-05-01 21:52:42 -0400

    merged molly's code

--- resources/app.js
+++ resources/app.js
@@ -47,11 +47,11 @@ $('#btn-search').click(function renderMap(e) {
 });
 
 // Matt's code
-$("#recently").on("click", function() {
+$("#history").on("click", function() {
   $("#xyz").css("display", "none")
   $("#recentDiv").css("display", "block")
 });
-  $("#history").on("click", function() {
+  $("#recently").on("click", function() {
     $("#recentDiv").css("display", "none")
     $("#xyz").css("display", "block")
   });
@@ -88,3 +88,106 @@ $("#btn-temp").on("click", function() {
      });
    });
 
+////////////////////////// GOOGLE MAPS GEOCODING API DATA /////////////////////////////////
+//to convert location user enters to lat/long
+
+//https://maps.googleapis.com/maps/api/geocode/json?address=Croatia&key=AIzaSyC38jvNaBiOYkmKPDHFXLYcOpdcJIqJ7PU
+
+var userAddress = "";
+var childrenArray = [];
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB4FzGqNZs6sYG5wsokxnFHJJutJSdbLTY",
+    authDomain: "tube-the-earth.firebaseapp.com",
+    databaseURL: "https://tube-the-earth.firebaseio.com",
+    projectId: "tube-the-earth",
+    storageBucket: "",
+    messagingSenderId: "686431765231"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+
+          //when data in database changes...
+          database.ref().on("child_added", function(childSnapshot) { //each time another search is added to database...
+            console.log("before push childrenArray",childrenArray.join(', '))
+            childrenArray.push(childSnapshot.val().userAddress); //push the new location the user entered to childrenArray
+            console.log("after push childrenArray",childrenArray.join(', '))
+            var numChildren = childrenArray.length; //increment # of children by one
+            if (numChildren < 5) { //if there are fewer than 5 children in database...
+                $("#recent-searches").html(childrenArray.join(', ')); //push updated contents of childrenArray to page
+            } else if (numChildren >= 5) {//otherwise, if there are 5+ children in database...  //NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                console.log("numchildren >= 5 childrenArray:",childrenArray.join(', '))
+                childrenArray.splice(0, 1, childSnapshot.val().userAddress); //replace oldest item in childrenArray with newest one
+                console.log("after splice childrenArray",childrenArray.join(', '))
+                $("#recent-searches").html(childrenArray.join(', ')); //push updated contents of childrenArray to page
+            }
+    });
+
+
+
+$("#search-button").on("click", function() {
+    userAddress = $("#search-field").val().trim(); // capturing user's entry in location field
+    event.preventDefault();
+
+//push data to firebase
+    database.ref().push({
+        userAddress: userAddress, //add jesse's var for location
+        });
+
+    
+    var apiKeyG = "AIzaSyC38jvNaBiOYkmKPDHFXLYcOpdcJIqJ7PU";
+    var urlG = "https://maps.googleapis.com/maps/api/geocode/json";
+    urlG = urlG + "?" + $.param({
+        'address': userAddress,
+        'key': apiKeyG
+    });
+
+$.ajax({
+    url: urlG,
+    method: "GET"
+}).then
+
+    (function(results) {
+        
+        var lat = results.results[0].geometry.location.lat;
+        var long = results.results[0].geometry.location.lng; //necessary to parse??
+
+///////////////////////////////////// YOUTUBE API DATA /////////////////////////////////
+
+//https://www.googleapis.com/youtube/v3/search?key=AIzaSyC3hyycsztOR8N1flGac1ocYQF1PGt6F6M&type=video&maxResults=5&part=snippet&chart=mostPopular&%E2%80%8Elocation=43.065041,-70.789078&videoCategoryId=10&videoEmbeddable=true
+
+    var apiKeyY = "AIzaSyC3hyycsztOR8N1flGac1ocYQF1PGt6F6M";
+    var urlY = "https://www.googleapis.com/youtube/v3/search";
+    urlY += "?" + $.param({
+        'type': 'video',
+        'maxResults': 5,
+        'part': 'snippet',
+        'videoEmbeddable': true,
+        'location': lat + "," + long,
+        'locationRadius': '10mi',
+        'key': apiKeyY,
+        'chart': 'mostPopular'
+    });
+
+    $.ajax({
+        url: urlY,
+        method: "GET"
+    }).then (function(results) {
+        $("#popular").empty();
+        for (i=0; i<5; i++) { //pushing 5 location-specific video thumbnails to page
+            var thumbnailPath = results.items[i].snippet.thumbnails.default.url;
+            var thumbnail = $("<img class='thumbnail'>").attr("src", thumbnailPath);
+            $("#popular").append(thumbnail);
+        }
+        });
+    });
+        
+
+  
+    });
+});
