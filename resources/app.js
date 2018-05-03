5e3f03514e364ca0605974055cf4ea89410771d5
“Steve <graphicdesigner@gmail.com>
2018-05-03 11:00:28 -0400

    UI refinements

--- resources/app.js
+++ resources/app.js
@@ -8,29 +8,6 @@ section.innerHTML =`
     src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBRjbBXmBByR-YaGaOMs7PAwS4SBmGIqzw&q=${searchTerm}" allowfullscreen>
     </iframe>`
 
-// assign variable to output
-
-// // put listener on button and when clicked do the function
-// $('#btn-search').on('click', renderMap(locationMap));
-
-// function renderMap(locationMap){
- 
-// console.log("greeting from inside the function" + locationMap);
-
-//  // clear out the map that's there
-//  UIframe.innerHTML = '';
-//  //the stuff we did up top
-
-// UIframe.innerHTML =`
-//     <iframe class="map" width="100%" height="100%" frameborder="0" style="border:0"
-//     src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBRjbBXmBByR-YaGaOMs7PAwS4SBmGIqzw&q=${locationMap}" allowfullscreen>
-//     </iframe>
-//                     `
-// alert("here");
-// // section.appendChild(UIframe);
-// // e.preventDefault();
-// }
-
 // put listener on button and when clicked do the function
 $('#btn-search').click(function renderMap(e) {
   e.preventDefault();
@@ -46,3 +23,22 @@ $('#btn-search').click(function renderMap(e) {
     </iframe>`
 })
 
+
+// Dabbling with jQuery UI elements
+$(document).ready(() => {
+
+$('.geo-recent-toggle').on('click', () => {
+  // Having trouble overriding the slim embed (i think) so I cant seem to swing any slides, toggles, or fades. frustrating.
+  // $('.geo-recent').slideToggle('fast');
+
+  $('.firebase').hide();
+  $('.geo-recent').show();
+});
+
+$('.firebase-toggle').on('click', () => {
+  // $('.firebase').fadeToggle(100);
+  $('.geo-recent').hide();
+  $('.firebase').show();
+});
+
+});
