274ece0f07aeb120024f9b83a26e5291385a85a5
Molly Zurheide <you@example.com>
2018-05-03 17:58:25 -0400

    more updates

--- app.js
+++ app.js
@@ -25,25 +25,15 @@ $(document).ready(function() {
     //when data in database changes, and on page load...
     database.ref().on("child_added", function(childSnapshot) {
                 
-        //each time another search is added to database... STILL NEED THIS PART TO PUSH NEW ITEMS TO DATABASE AS THEY'RE ADDED!!!!!!!!!!!!!!!!!!!!!! BUT 
-        //childrenArray.push(childSnapshot.val().userAddress); //push the new location the user entered to childrenArray
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//FOR ANY LOCATION IN DATABASE, ASSIGN LAT/LONG
-//can't do globally because need ajax call
-//can't do inside other ajax call because need the info to be available before click event, and because can't reference childArray in that scope? COULD I JUST CREATE A NEW ARRAY THERE????
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-console.log("wtf")
+        childrenArray.push(childSnapshot.val().userAddress); //push the new location the user entered to childrenArray
+//BUT DON'T WANT THIS PRINTED TO PAGE, JUST ADDED TO ARRAY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     });
 
         $("#search-button").on("click", function() {
             userAddress = $("#search-field").val().trim(); // capturing user's entry in location field
             event.preventDefault();
-//push data to firebase
+
+        //push data to firebase
             database.ref().push({
                 userAddress: userAddress, //add jesse's var for location
             });
@@ -59,7 +49,7 @@ console.log("wtf")
                 url: urlG,
                 method: "GET"
                 }).then
-                (function(results) { //****************** */
+                (function(results) {
                     var lat = results.results[0].geometry.location.lat;
                     var long = results.results[0].geometry.location.lng;
 
@@ -83,10 +73,10 @@ console.log("wtf")
     $.ajax({
         url: urlY,
         method: "GET"
-    }).then (function(results) {
+    }).then (function(response) {
         $("#user-results").empty();
         for (var i=0; i<5; i++) { //pushing 5 location-specific video thumbnails to page
-            var userThumbnailPath = results.items[i].snippet.thumbnails.default.url;
+            var userThumbnailPath = response.items[i].snippet.thumbnails.default.url;
             var userThumbnail = $("<img class='user-thumbnail'>").attr("src", userThumbnailPath);
             $("#user-results").append(userThumbnail);
         }
@@ -99,14 +89,54 @@ console.log("wtf")
         var popularSearchesArray = [];
         var query = firebase.database().ref().orderByKey(); //grabbing data from firebase
         query.once("value")
-            .then(function(snapshot) {
-                snapshot.forEach(function(childSnapshot) { //grab value of each child item in database 
-                var popularSearchItem = childSnapshot.val();
-                console.log(popularSearchItem);
-                var length = snapshot.numChildren();
-                console.log(length);
-                popularSearchesArray.push(popularSearchItem);
-                //convert to lat-long
+            .then(function(snapshot, childSnapshot) {
+              
+              
+              
+              
+                snapshot.forEach(function(childSnapshot) { //for each child in database...
+                    var popularSearchItem = childSnapshot.val(); //grab value
+                    var length = snapshot.numChildren(); //grab # of children in database JUST SNAPSHOT????????????????????????
+                    popularSearchesArray.push(popularSearchItem); //push each child to array
+
+//JSON PARSE EACH CHILD SNAPSHOT LOCATION????
+
+
+
+
+                var apiKeyH = "AIzaSyC38jvNaBiOYkmKPDHFXLYcOpdcJIqJ7PU";
+                var urlH = "https://maps.googleapis.com/maps/api/geocode/json";
+                urlH = urlH + "?" + $.param({
+                    'address': childSnapshot.val(),//??????????????????? I WANT DATA FROM DATABASE, NOT FROM USER'S SEARCH!!!!!!!!!!!!!!!!!!!!!!!!!!!! BUT DON'T WANT *ALL* ADDRESSES, JUST WANT ONE AT A TIME!!!!!!!!!!!!!!!!!!!!!!!
+                    'key': apiKeyH
+                });
+
+
+                $.ajax({
+                    url: urlH,
+                    method: "GET"
+                }).then
+                    (function(results) {                
+                        //console.log(results);
+                        var popularLat = results.results[0].geometry.location.lat;
+                        var popularLong = results.results[0].geometry.location.lng;
+                            var popularLatString = popularLat.toString();
+                            var popularLongString = popularLong.toString();
+                            var latLongArray = [];
+                            var popularLatLong = popularLatString + "," + popularLongString;                             
+                        });
+                        //NOW ANOTHER YOUTUBE AJAX CALL????????????????????????????? BUT HOW GET LAT/LONG THERE??????????????????????????
+                  //console.log(popularSearchesArray.length);
+//                        for (var x=0; x<popularSearchesArray.length; x++) {
+                    //for each search item in database, convert to lat/long pair
+
+                   //var popularLat = ;
+                    //var popularLong = ;
+                    //latLongArray.push(___);
+
+                    });
+
+
                 //push thumbnail
                 var numChildren = popularSearchesArray.length;
                 if (numChildren < 6) {
@@ -121,6 +151,8 @@ console.log("wtf")
         });
 
 
+//results.results[0].geometry.location.lat
+
 /*
         $("#popular-results").empty();
         for (var x=0; x<5; x++) { //pushing video thumbnail to page for each item already in array
@@ -134,4 +166,8 @@ console.log("wtf")
     });
         });
     });
-    });
+
+
+    /*
+    https://maps.googleapis.com/maps/api/geocode/json?address%5B-LBbslOsDqarhYO3C8B8%5D%5BuserAddress%5D=asia&address%5B-LBbstzKOau4rwXXsu6X%5D%5BuserAddress%5D=oregon&address%5B-LBbsxfWEjHsnbRCcASN%5D%5BuserAddress%5D=arkansas&address%5B-LBbtkbQknMy_WdypQAY%5D%5BuserAddress%5D=alabama&address%5B-LBbtriZoevJAE_v6HTE%5D%5BuserAddress%5D=alaska&key=AIzaSyC38jvNaBiOYkmKPDHFXLYcOpdcJIqJ7PU
+    */
