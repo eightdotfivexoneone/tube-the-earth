ae52165ddfda0dc7da46c45d71a9e3bf8268886e
Matt Eisenstadter <mattieice@gmail.com>
2018-05-04 17:00:20 -0400

    matt branch merge

--- resources/app.js
+++ resources/app.js
@@ -1,81 +1,117 @@
-// JS for Tube The Earth
-const section = document.getElementById("map-section");
+      var longi;
+      var lati;
+  
+      function initMap() {
+        
+        var map = new google.maps.Map(document.getElementById('map-section'), {
+          zoom: 8,
+          center: {lat: 43.07167, lng: -70.76236}
+        });
 
-var searchTerm = "03801";
+        var geocoder = new google.maps.Geocoder();
+        console.log(map.center);
+        google.maps.event.addListener(map, 'click', function(event) {
+          placeMarker(event.latLng);
+        });
 
-section.innerHTML =`
-    <iframe class="map" width="100%" height="100%" frameborder="0" style="border:0"
-    src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBRjbBXmBByR-YaGaOMs7PAwS4SBmGIqzw&q=${searchTerm}" allowfullscreen>
-    </iframe>`
+        function placeMarker(location) {
+            if (marker == undefined){
+                marker = new google.maps.Marker({
+                    position: location,
+                    map: map, 
+                    animation: google.maps.Animation.DROP,
+                });
+            }
+            else{
+                marker.setPosition(location);
+            }
+            map.setCenter(location);
+            console.log(location.lat());//this is where latitude is being console logged
+            console.log(location.lng());//longitude
+        }
 
-// assign variable to output
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+          console.log(geocoder);
+          console.log(map);
+          console.log(map.center.lat());
+          console.log(map.center.lng());
+          longi = map.center.lng();
+          lati = map.center.lat();
+          //debugger;
+          // var latitude = $("#lat").val().trim();
+          // var longitude = $("#long").val().trim();
+          var radius = $("#rad").val().trim();
+          var queryURL = "https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&location=" + lati + "," + longi + "&locationRadius=" + radius + "miles&key=AIzaSyC3hyycsztOR8N1flGac1ocYQF1PGt6F6M";
+          console.log(lati);
+          console.log(longi);
+          $.ajax({
+          url: queryURL,
+          method: "GET"
+          })
+          
+          .then(function(response) {
+            var results = response.data;
+            console.log(response);
+            console.log(response.pageInfo.totalResults);
+            if (response.pageInfo.totalResults !== 0) {
+              for (var i = 0; i < response.items.length; i++) {             
 
-// // put listener on button and when clicked do the function
-// $('#btn-search').on('click', renderMap(locationMap));
+                console.log(response);
+                console.log(response.items[i].id.videoId);
+                
+                $("#cardThumb" + i).html("<iframe width='100%' src='https://www.youtube.com/embed/" + response.items[i].id.videoId + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>");
+                $("#youtubeLink" + [i]).html("<a href='https://www.youtube.com/watch?v=" + response.items[i].id.videoId + "' class='btn btn-danger' target='_blank'>" + response.items[i].snippet.channelTitle + "</a>");
+                // $("#youtubeTitle" + [i]).html(response.items[i].snippet.title);
+              }
+              $("#videoIframe").html("<iframe width='100%' src='https://www.youtube.com/embed/" + response.items[0].id.videoId + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>");
+            } else {
+                alert("no results!");
+              };
+            });
+          });
+      };
+      
+      console.log(longi);
+        
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            // alert('Geocode was not successful for the following reason: ' + status);
+          }
+          console.log(address);
+          console.log(results[0].geometry.location.lat());
+          console.log(results[0].geometry.location.lng());
+        });
+      }
 
-// function renderMap(locationMap){
- 
-// console.log("greeting from inside the function" + locationMap);
+      var marker;
 
-//  // clear out the map that's there
-//  UIframe.innerHTML = '';
-//  //the stuff we did up top
+      function initialize() {
+          var latlng = new google.maps.LatLng(42.55308, 9.140625);
+          var myOptions = {
+              zoom: 2,
+              center: latlng,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              streetViewControl: false,
+              mapTypeControl: false,
+          };
+          var map = new google.maps.Map(document.getElementById("map_canvas"),
+                  myOptions);
+      };
 
-// UIframe.innerHTML =`
-//     <iframe class="map" width="100%" height="100%" frameborder="0" style="border:0"
-//     src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBRjbBXmBByR-YaGaOMs7PAwS4SBmGIqzw&q=${locationMap}" allowfullscreen>
-//     </iframe>
-//                     `
-// alert("here");
-// // section.appendChild(UIframe);
-// // e.preventDefault();
-// }
-
-// put listener on button and when clicked do the function
-$('#btn-search').click(function renderMap(e) {
-  e.preventDefault();
-  var locationMap = $("#search-input").val();
-
- // clear out the map that's there
- section.innerHTML = '';
- //the stuff we did up top
-
- section.innerHTML =`
-    <iframe class="map" width="100%" height="100%" frameborder="0" style="border:0"
-    src="http://www.google.com/maps/embed/v1/search?key=AIzaSyBRjbBXmBByR-YaGaOMs7PAwS4SBmGIqzw&q=${locationMap}"allowfullscreen>
-    </iframe>`
-});
-
-// Matt's code
-$("#btn-temp").on("click", function() {
-   var latitude = $("#lat").val().trim();
-   var longitude = $("#long").val().trim();
-   var radius = $("#rad").val().trim();
-   var queryURL = "https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&location=" + latitude + "," + longitude + "&locationRadius=" + radius + "miles&key=AIzaSyC3hyycsztOR8N1flGac1ocYQF1PGt6F6M";
-
-   $.ajax({
-   url: queryURL,
-   method: "GET"
-   })
-   
-   .then(function(response) {
-     var results = response.data;
-     console.log(response);
-     console.log(response.pageInfo.totalResults);
-     if (response.pageInfo.totalResults !== 0) {
-       for (var i = 0; i < response.items.length; i++) {             
-
-         console.log(response);
-         console.log(response.items[i].id.videoId);
-         
-         $(".cardThumb").html("<img src=" + response.items[i].snippet.thumbnails.high.url + ">");
-         $("#youtubeLink1").html("<a href='https://www.youtube.com/watch?v=" + response.items[i].id.videoId + "'>youtube link</a>");
-         $("#youtubeTitle1").html(response.items[i].snippet.title);
-         $("#videoIframe").html("<iframe width='420' height='315' src='https://www.youtube.com/embed/" + response.items[0].id.videoId + "' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>");
-       }
-     } else {
-         alert("no results!");
-       };
-     });
-   });
-
+      $("#history").on("click", function() {
+        $("#additionalDiv").css("display", "none")
+        $("#historyDiv").css("display", "block")
+      });
+      $("#recently").on("click", function() {
+          $("#historyDiv").css("display", "none")
+          $("#additionalDiv").css("display", "block")
+      });
